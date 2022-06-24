8ce159aedc1ca239074cc5526d08f9545c016899
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-24 12:54:33 +0500

    Added child model

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,2 +1,7 @@
 class Parent < ApplicationRecord
+  has_many :children, dependent: :destroy
+
+  validates :name, :phone_number, presence: true
+  validates :email,:phone_number, uniqueness: true
+  validates :email, format: {with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ , message: "Please enter a valid email address."}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_24_052437) do
+ActiveRecord::Schema.define(version: 2022_06_24_053424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "children", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.bigint "parent_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["parent_id"], name: "index_children_on_parent_id"
+  end
 
   create_table "parents", force: :cascade do |t|
     t.string "name"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2022_06_24_052437) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "children", "parents"
 end
