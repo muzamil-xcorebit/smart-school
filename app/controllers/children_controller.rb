7e05984c8a598219a45e513897bee46fe8046337
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-06-30 14:03:45 +0500

    Added children controller and views

---
+++ app/controllers/children_controller.rb
@@ -0,0 +1,55 @@
+class ChildrenController < ApplicationController
+  before_action :set_parent, only: %i[index create new]
+  before_action :set_child, only: %i[show edit update destroy]
+
+  def index
+    @children = @parent.children
+  end
+
+  def new
+    @child = @parent.children.build
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create
+    @child = @parent.children.build(child_params)
+    if @child.save
+      redirect_to parent_children_path
+    else
+      render'new'
+    end
+  end
+
+  def update
+    if @child.update(child_params)
+      redirect_to @child
+    else
+      render'edit'
+    end
+  end
+
+  def destroy
+    @child.destroy
+    redirect_to parent_children_path([current_parent, @child])
+  end
+
+  private
+
+  def set_parent
+    @parent = Parent.find(params[:parent_id])
+  end
+
+  def set_child
+    @child = Child.find(params[:id])
+  end
+
+  def child_params
+    params.require(:child).permit(:name, :age)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :parents, controllers: { confirmations: 'parents/confirmations' }
   resources :pods
+  resources :parents do
+    resources :children, shallow: true
+  end
   root 'pods#index'
 end
