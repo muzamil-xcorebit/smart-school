3e001110fcee76fa16b4b4ab916512a5e7de15f1
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-07-14 16:08:09 +0500

    Added spaces

--- app/controllers/child_pods_controller.rb
+++ app/controllers/child_pods_controller.rb
@@ -1,15 +1,18 @@
 class ChildPodsController < ApplicationController
   after_action :send_email_to_admin, only: [:create]
+
   def new
     @pod = Pod.find(params[:id])
     @child_pod = ChildPod.new
   end
+
   def create
     params[:children_ids]&.each do |cid|
       ChildPod.create(child_id: cid, pod_id: params[:child_pod][:pod_id])
     end
     redirect_to pods_path
   end
+
   def send_email_to_admin
     ChildPodsMailer.with(
       parent: current_parent.email,
@@ -17,4 +20,5 @@ class ChildPodsController < ApplicationController
       pod: params[:child_pod][:pod_id]
       ).child_pod_created.deliver_later
   end
+
 end
