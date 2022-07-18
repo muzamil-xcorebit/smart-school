fde940fc2372d537aa80ba990fba55a88a464452
Muzamil Hussain <muzamil.hussain@xcorebit.com>
2022-07-18 14:58:14 +0500

    Updated child pods history

--- app/models/child.rb
+++ app/models/child.rb
@@ -14,5 +14,9 @@ class Child < ApplicationRecord
   has_many :active_pods, ->   { where(child_pods: { status: 'active'   }) }, through: :child_pods, source: :pod
   has_many :rejected_pods, -> { where(child_pods: { status: 'rejected' }) }, through: :child_pods, source: :pod
   has_many :inactive_pods, -> { where(child_pods: { status: 'inactive' }) }, through: :child_pods, source: :pod
-  scope :active_pod, -> { active_pods.last }
+  
+  def active_pod
+    active_pods.last
+  end
+
 end
